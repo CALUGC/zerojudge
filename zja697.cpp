ab1777ff2b599dc2d174206dc172ebf625888664
CALUGC <44196124+CALUGC@users.noreply.github.com>
2019-02-25 17:12:36 +0800

    Update zja697.cpp

--- zja697.cpp
+++ zja697.cpp
@@ -1,31 +1,26 @@
 #include <cstdio>
-#define f(x,i) for(int i=0;i<x;i++)
+#define f(x,i) for (int i = 0; i < x; ++i)
 
 int main(int argc, char const *argv[])
 {
-	int n,m;
+	int n,m,sqar[505][505];
 	while(~scanf("%d %d",&n,&m)){
-		int sqar[n][n],x1,y1,x2,y2,sum=0;
+
+		int x1,y1,x2,y2;
 		f(n,i)
 			f(n,j)
 				scanf("%d",&sqar[i][j]);
-		//1<=n
-		f(m,k){
-			sum=0;
+
+		while(m--){
+			int sum=0;
 			scanf("%d %d %d %d",&x1,&y1,&x2,&y2);
-			for (int i = y1-1; i <= y2-1; ++i)
-			{
-				for (int j = x1-1; j <= x2-1; ++j)
-				{
-					sum+=sqar[i][j];
-				}
 
-			}
-			printf("aa%daa\n",sum );
+			for (int i = y1-1; i <=y2-1 ; ++i)
+				for (int j = x1-1; j <=x2-1 ; ++j)
+					sum+=sqar[j][i];
+			printf("%d\n",sum );
+
 		}
-		
-
-
 	}
 	return 0;
 }
